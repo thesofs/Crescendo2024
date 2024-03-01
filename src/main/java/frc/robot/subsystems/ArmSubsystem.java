e80b250b0884797aa3f1e4e65d4095e3802ec931
Karya Basaraner <kBasaraner24@bfhsla.org>
2024-02-29 18:31:28 -0600

    WIP: Feed Forward Control
    Co-authored-by: Bennett Hermanoff <bennett@hermanoff.dev>

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -9,6 +9,7 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
@@ -16,24 +17,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 
 public class ArmSubsystem extends ProfiledPIDSubsystem {
-  public final CANSparkMax m_spark = new CANSparkMax(11,MotorType.kBrushless);
+  public final CANSparkMax m_spark = new CANSparkMax(11, MotorType.kBrushless);
   public final CANSparkMax m_spark2 = new CANSparkMax(12, MotorType.kBrushless);
-  //public final RelativeEncoder encoder_arm = m_spark.getAlternateEncoder();
-  //public final CANSparkMax hand_spark = new CANSparkMax(21,MotorType.kBrushless);
+  private ArmFeedforward armFFController;
+  // public final RelativeEncoder encoder_arm = m_spark.getAlternateEncoder();
+  // public final CANSparkMax hand_spark = new
+  // CANSparkMax(21,MotorType.kBrushless);
   public final RelativeEncoder alternateEncoder;
-  //public final DutyCycleEncoder dutyCycleEncoder;
-  //private final double ENCODER_OFFSET = -1.416992;
-  //encoder_arm.setDistancePerPulse(1.0 / 360.0 * 2.0 * Math.PI * 1.5);
+  // public final DutyCycleEncoder dutyCycleEncoder;
+  // private final double ENCODER_OFFSET = -1.416992;
+  // encoder_arm.setDistancePerPulse(1.0 / 360.0 * 2.0 * Math.PI * 1.5);
 
-  
   /** Creates a new Arm. */
 
-  //public final CANSparkMax spark = new CANSparkMax (14, MotorType.kBrushless);
- // public final RelativeEncoder encoderS = spark.getEncoder();
-    
-  
-  public ArmSubsystem() {
+  // public final CANSparkMax spark = new CANSparkMax (14, MotorType.kBrushless);
+  // public final RelativeEncoder encoderS = spark.getEncoder();
 
+  public ArmSubsystem() {
 
     super(
         // The ProfiledPIDController used by the subsystem
@@ -44,46 +44,51 @@ public class ArmSubsystem extends ProfiledPIDSubsystem {
             // The motion profile constraints
             new TrapezoidProfile.Constraints(200, 200)));
     this.alternateEncoder = m_spark.getAlternateEncoder(8192);
+    /**
+     * double ka
+     * The acceleration gain, in volt seconds² per radian.
+     * double kg
+     * The gravity gain, in volts.
+     * double ks
+     * The static gain, in volts.
+     * double kv
+     * The velocity gain, in volt seconds per radian.
+     */
+    double kS = 0;
+    double kG = 0;
+    double kV = 0;
+    this.armFFController = new ArmFeedforward(kS, kG, kV, 0);
 
-
-  
     m_spark.setIdleMode(IdleMode.kBrake);
     m_spark2.setIdleMode(IdleMode.kBrake);
-    
-    m_spark2.follow(m_spark,true);
 
+    m_spark2.follow(m_spark, true);
 
   } // what is Kdt?
 
   @Override
   public void useOutput(double output, TrapezoidProfile.State setpoint) {
-    // Use the output (and optionally the setpoint) here
-      // double invertedOutput = output * -1;
-  SmartDashboard.putNumber("Arm Output", output);
-  // SmartDashboard.putNumber("Inverted Arm Output", invertedOutput);
-   m_spark.set(output);
-   //m_spark2.set(output);
- 
-  // hand_spark.set(output);
-  }
+    SmartDashboard.putNumber("Arm Output", output);
 
+    double feedforward = armFFController.calculate(setpoint.position,0);
+    m_spark.set(output+feedforward);
+   
+  }
 
   // double kgOutput = Math.cos(setpoint.position);
   // m_spark.set(kgOutput + output);
+  
 
-   public double getArmEncoderPos(){ // arm deg
+  public double getArmEncoderPos() { // arm deg
 
-   return (-alternateEncoder.getPosition())*12/52*360; //Put encoder offset back in 
+    return (-alternateEncoder.getPosition()) * 12 / 52 * 360; // Put encoder offset back in
 
- }
-
-
+  }
 
   @Override
   public double getMeasurement() {
     // Return the process variable measurement here
     return getArmEncoderPos();
-    
-    
+
   }
 }
